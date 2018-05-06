f4e09f78b33628b1fc4a31dc5265c7b8861b14fb
Denis Wolf <i-wild-wolf@yandex.ru>
2018-05-06 11:01:49 +0300

    Переработка GuesNumber в методы

--- lesson 1/GuesRandomNumber.java
+++ lesson 1/GuesRandomNumber.java
@@ -2,65 +2,87 @@ import java.io.*;
 import java.util.*;
 
 public class GuesRandomNumber {
-    static int randomNumber;
+    private static int randomNumber;
+    private static boolean isStopped = false;
+
+
     public static void main(String args[]) throws Exception {
-        System.setProperty("console.encoding","UTF-8");
+        //System.setProperty("console.encoding","UTF-8");
 
         print("Добро пожаловать в угадайку.\n(Для выхода введите exit)\n");
 
-        do {
+        while (!isStopped) {
             randomNumber = getRandomNumber();
             print("Привет! Я загадал число от 0 до 100. Угадай какое?");
-            game();
-        } while (wantToPlayAgain());
+            play();
+            wantToPlayAgain();
+        }
 
     }
 
-    public static void print(Object obj) {
+    private static void print(Object obj) {
         System.out.println(obj);
     }
 
-    public static int getRandomNumber() {
-        Long seed = new Date().getTime();
+    private static String takeUserInput() throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String inputData = reader.readLine();
+        if (inputData.equalsIgnoreCase("exit")) {
+            isStopped = true;
+            print("Приходи ещё! Я буду тебя ждать)")
+        }
+        return  inputData;
+    }
+
+    private static int getRandomNumber() {
+        long seed = new Date().getTime();
         Random random = new Random(seed);
         return random.nextInt(100);
     }
 
-    public static void game() throws Exception {
-        while(!checkUserNumberAnswer(inputRequest())) {
-        }
-    }
-
-    public static boolean checkUserNumberAnswer (int answer) {
-        if (answer > randomNumber) {
-            print("Нет, я загадал число поменьше.");
-        } else if (answer < randomNumber) {
-            print("Нет, я загадал число побольше.");
-        } else if (answer == randomNumber) {
-            print("Ух-Ты! Ты угадал моё число!!!");
-            return true;
-        }
-        return false;
-    } 
-
-    public static int inputRequest() throws Exception {
-        String input = (new BufferedReader(new InputStreamReader(System.in))).readLine();
-        if (input.equalsIgnoreCase("exit")) {
-            System.exit(0);
-        } else {
-            try {
-                return Integer.parseInt(input);
-            } catch (NumberFormatException nfex) {
-                return inputRequest();
+    private static void play() throws Exception {
+        while(!isStopped) {
+            if(checkUserNumberAnswer(numberRequest())) {
+                break;
             }
         }
-        return 0;
     }
 
-    public static boolean wantToPlayAgain () throws Exception {
-        print("Хочешь сыграть ещё раз?");
-        print("Введи \"да\", если хочешь");
-        return (new BufferedReader(new InputStreamReader(System.in))).readLine().equalsIgnoreCase("да");
+    private static boolean checkUserNumberAnswer (int answer) {
+        if (isStopped) {
+            return false;
+        } else {
+            if (answer > randomNumber) {
+                print("Нет, я загадал число поменьше.");
+            } else if (answer < randomNumber) {
+                print("Нет, я загадал число побольше.");
+            } else if (answer == randomNumber) {
+                print("Ух-Ты! Ты угадал моё число!!!");
+                return true;
+            }
+            return false;
+        }
+    }
+
+    private static int numberRequest() throws Exception {
+        if (isStopped) {
+            return -1;
+        } else {
+            try {
+                return Integer.parseInt(takeUserInput());
+            } catch (NumberFormatException nfex) {
+                return numberRequest();
+            }
+        }
+    }
+
+    private static void wantToPlayAgain () throws Exception {
+        if (!isStopped) {
+            print("Хочешь сыграть ещё раз?");
+            print("Введи \"yes\", если хочешь");
+            isStopped = !(takeUserInput().equalsIgnoreCase("yes"));
+        }
     }
 
 }
